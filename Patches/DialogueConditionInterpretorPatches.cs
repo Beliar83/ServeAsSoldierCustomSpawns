22996dba41d1f769fc1518358b7defdae6a5139b
Karsten Bock <KarstenBock@gmx.net>
2024-09-11 20:08:31 +0200

    Fix crash after town robbery event

---
+++ Patches/DialogueConditionInterpretorPatches.cs
@@ -0,0 +1,50 @@
+﻿using CustomSpawns.Dialogues;
+using CustomSpawns.Utils;
+using HarmonyLib;
+
+namespace ServeAsSoldierCustomSpawns.Patches;
+
+[HarmonyPatch(typeof(DialogueConditionInterpretor))]
+public static class DialogueConditionInterpretorPatches
+{
+    [HarmonyPatch("HasPartyID")]
+    [HarmonyPrefix]
+    private static bool HasPartyIDPrefix(out bool __result, DialogueParams param, string ID)
+    {
+        // ReSharper disable ConditionalAccessQualifierIsNonNullableAccordingToAPIContract
+        __result = param.AdversaryParty?.Party?.MobileParty != null &&
+                   CampaignUtils.IsolateMobilePartyStringID(param.AdversaryParty.Party.MobileParty) == ID;
+        // ReSharper restore ConditionalAccessQualifierIsNonNullableAccordingToAPIContract
+        return false;
+    }
+
+    [HarmonyPatch("PartyIsInFaction")]
+    [HarmonyPrefix]
+    private static bool PartyIsInFactionPrefix(out bool __result, DialogueParams param, string factionID)
+    {
+        // ReSharper disable ConditionalAccessQualifierIsNonNullableAccordingToAPIContract
+        __result = param.AdversaryParty?.MapFaction != null && param.AdversaryParty.MapFaction.StringId == factionID;
+        // ReSharper restore ConditionalAccessQualifierIsNonNullableAccordingToAPIContract
+        return false;
+    }
+
+    [HarmonyPatch("IsFriendly")]
+    [HarmonyPrefix]
+    private static bool IsFriendlyPrefix(out bool __result, DialogueParams param)
+    {
+        // ReSharper disable ConditionalAccessQualifierIsNonNullableAccordingToAPIContract
+        __result = !param.AdversaryParty?.MapFaction?.IsAtWarWith(param.PlayerParty?.MapFaction) ?? false;
+        // ReSharper restore ConditionalAccessQualifierIsNonNullableAccordingToAPIContract
+        return false;
+    }
+
+    [HarmonyPatch("IsHostile")]
+    [HarmonyPrefix]
+    private static bool IsIsHostilePrefix(out bool __result, DialogueParams param)
+    {
+        // ReSharper disable ConditionalAccessQualifierIsNonNullableAccordingToAPIContract
+        __result = param.AdversaryParty?.MapFaction?.IsAtWarWith(param.PlayerParty?.MapFaction) ?? false;
+        // ReSharper restore ConditionalAccessQualifierIsNonNullableAccordingToAPIContract
+        return false;
+    }
+}
